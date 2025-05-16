f75e71200e510d1ce9ac170c3f9813bf150a9424
hotzobori <hotzobori@gmail.com>
2025-05-17 00:33:44 +0900

    setup : create portal be/fe packages

---
+++ services/portal/backend/portal/src/main/java/co/hotzo/portal/PortalApplication.java
@@ -0,0 +1,13 @@
+package co.hotzo.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalApplication.class, args);
+    }
+
+}
